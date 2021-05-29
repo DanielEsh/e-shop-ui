b99a9c81d318d84af8a2049ffdb4421c41bd9796
Daniel <daniilterawars@gmail.com>
2021-05-29 22:20:31 +0300

    feat(colors): add colors

---
+++ src/components/Colors/Colors.stories.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Story, Meta } from '@storybook/react';
+
+import Colors from "./index";
+
+export default {
+    title: 'Example/Colors',
+    component: Colors,
+} as Meta;
+
+const Template = (args) => <Colors {...args} />;
+
+export const All = Template.bind({});
+All.args = {};
---
+++ src/components/Theme/index.tsx
@@ -0,0 +1,20 @@
+const theme = {
+    colors: {
+        primary: '#5865f2',
+        primaryHover: '#424cba',
+        success: '#00c851',
+        successHover: '#198754',
+        warning: '#fb3',
+        warningHover: '#f8a808',
+        danger: '#ff3547',
+        dangerHover: '#dc3545',
+        info: '#33b5e5',
+        infoHover: '#0dcaf0',
+        white: '#fcfcfc',
+        whiteHover: '#f7f7f7',
+        black: '#2d2f33',
+        blackHover: '#3f4246'
+    }
+}
+
+export default theme;
