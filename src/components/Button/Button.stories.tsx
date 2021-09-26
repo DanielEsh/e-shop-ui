f390d08a520d71d33fa767f47c0a43116959edea
Daniel <daniilterawars@gmail.com>
2021-09-26 19:48:30 +0300

    feat(Button): add stories

--- src/components/Button/Button.stories.tsx
+++ src/components/Button/Button.stories.tsx
@@ -8,62 +8,44 @@ export default {
     component: Button,
 } as Meta;
 
-export const PrimaryMedium = () => {
-    return (
-        <Button>
-            Button
-        </Button>
-    )
-}
+const ButtonTemplate: Story<ButtonProps> = (args) => <Button { ...args } />;
 
-export const RoundedOutline = () => {
-    return (
-        <Button
-            outline
-            rounded
-            size={ 'medium' }
-        >
-            Button
-        </Button>
-    )
-}
+export const Default = ButtonTemplate.bind({});
+Default.args = { 
+    children: 'Default button'
+};
 
-export const SuccessLarge = () => {
-    return (
-        <Button
-            color={ 'success' }
-            outline
-            rounded
-            size={ 'large' }
-        >
-            Button
-        </Button>
-    )
-}
+export const RoundedOutline = ButtonTemplate.bind({});
+RoundedOutline.args = { 
+    children: 'RoundedOutline button',
+    outline: true,
+    rounded: true,
+};
 
-export const DarkLarge = () => {
-    return (
-        <Button
-            color={ 'dark' }
-            outline
-            rounded
-            size={ 'large' }
-        >
-            Button
-        </Button>
-    )
-}
+export const SuccessLarge = ButtonTemplate.bind({});
+SuccessLarge.args = { 
+    color: 'success',
+    size: 'large',
+    children: 'RoundedOutline button',
+    outline: true,
+    rounded: true,
+};
 
-export const Disabled = () => {
-    return (
-        <Button
-            color={ 'dark' }
-            disabled
-            outline
-            rounded
-            size={ 'large' }
-        >
-            Button
-        </Button>
-    )
-}
+export const Dark = ButtonTemplate.bind({});
+Dark.args = { 
+    color: 'dark',
+    size: 'large',
+    children: 'RoundedOutline button',
+    outline: true,
+    rounded: true,
+};
+
+export const Disabled = ButtonTemplate.bind({});
+Disabled.args = { 
+    color: 'dark',
+    size: 'large',
+    children: 'RoundedOutline button',
+    outline: true,
+    rounded: true,
+    disabled: true,
+};
