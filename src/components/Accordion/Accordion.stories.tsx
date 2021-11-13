ce822cc8fb77c1d2ecde5ce987bd8be93409c43f
Daniel <daniilterawars@gmail.com>
2021-11-13 19:45:39 +0300

    feat(Accordion): add accordion

--- src/components/Accordion/Accordion.stories.tsx
+++ src/components/Accordion/Accordion.stories.tsx
@@ -1,48 +1,11 @@
 import React from 'react';
 import { Story, Meta } from '@storybook/react';
 
-import { Accordion, AccordionProps } from './index';
+import {Accordion} from './index';
 
 export default {
     title: 'Components/Accordion',
     component: Accordion,
-    argTypes: { onClick: { action: 'clicked' } },
 } as Meta;
 
-const Template: Story<AccordionProps> = (args) => <Accordion { ...args } />;
-
-export const Default = Template.bind({});
-Default.args = {
-    header: 'AccordionItem',
-    content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio quis adipisci id rem veritatis suscipit consequuntur molestiae odit vitae provident aliquam reiciendis hic, blanditiis molestias est qui iure voluptates sunt.'
-};
-
-export const ActiveDefault = Template.bind({});
-ActiveDefault.args = {
-    header: 'AccordionItem',
-    content: `Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-    Odio quis adipisci id rem veritatis suscipit consequuntur molestiae
-    odit vitae provident aliquam reiciendis hic, blanditiis molestias est qui iure voluptates sunt.
-    Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-    Odio quis adipisci id rem veritatis suscipit consequuntur molestiae
-    odit vitae provident aliquam reiciendis hic, blanditiis molestias est qui iure voluptates sunt.
-    Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-    Odio quis adipisci id rem veritatis suscipit consequuntur molestiae
-    odit vitae provident aliquam reiciendis hic, blanditiis molestias est qui iure voluptates sunt.`,
-    active: true,
-};
-
-export const Disabled = Template.bind({});
-Disabled.args = {
-    header: 'AccordionItem',
-    content: `Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-    Odio quis adipisci id rem veritatis suscipit consequuntur molestiae
-    odit vitae provident aliquam reiciendis hic, blanditiis molestias est qui iure voluptates sunt.
-    Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-    Odio quis adipisci id rem veritatis suscipit consequuntur molestiae
-    odit vitae provident aliquam reiciendis hic, blanditiis molestias est qui iure voluptates sunt.
-    Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-    Odio quis adipisci id rem veritatis suscipit consequuntur molestiae
-    odit vitae provident aliquam reiciendis hic, blanditiis molestias est qui iure voluptates sunt.`,
-    disabled: true,
-};
+export { default as Default} from './examples/Default';
