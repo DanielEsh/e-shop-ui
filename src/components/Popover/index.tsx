b98dbdf0cb05ac2cf874bedde3f27f765e125111
Daniel <daniilterawars@gmail.com>
2022-01-04 03:58:38 +0300

    feat: try use floating ui

--- src/components/Popover/index.tsx
+++ src/components/Popover/index.tsx
@@ -1,5 +1,6 @@
 import React, {FC, ReactElement, useRef, useEffect} from 'react';
 import { usePopover } from '../../hooks/usePopover';
+import {useFloating, flip, getScrollParents} from '@floating-ui/react-dom';
 
 import {
     PopoverContainer,
@@ -47,25 +48,65 @@ export const Popover:FC<PopoverProps> = (props) => {
     // const ctnt = useRef(null);
     // const arrow = useRef(null);
 
-    let a,b;
+    let a, b;
 
-    const { styles, activator, popper } = usePopover({
-        placement: placement,
-        offsetY,
-        offsetX,
+    // const { styles, activator, popper } = usePopover({
+    //     placement: placement,
+    //     offsetY,
+    //     offsetX,
+    // });
+
+    const {x, y, reference, floating, strategy, update, refs} = useFloating({
+        placement: 'top',
+        middleware: [flip()],
     });
+
+    useEffect(() => {
+        if (!refs.reference.current || !refs.floating.current) {
+          return;
+        }
+        const parents = [
+          ...getScrollParents(refs.reference.current),
+          ...getScrollParents(refs.floating.current),
+        ];
+        parents.forEach((parent) => {
+          parent.addEventListener('scroll', update);
+          parent.addEventListener('resize', update);
+        });
+        return () => {
+          parents.forEach((parent) => {
+            parent.removeEventListener('scroll', update);
+            parent.removeEventListener('resize', update);
+          });
+        };
+      }, [refs.reference, refs.floating, update]);
 
     return (
         <>
-            <div ref={ activator }>
+            <div ref={ reference }>
                 { children }
             </div>
             <Portal container={ attachEl ? attachEl : null }>
-                {
+                {/* {
                     isVisible && (
                         <PopoverContainer 
                             ref={ popper }
                             style={ styles }
+                        >
+                            {content}
+                        </PopoverContainer>
+                    )
+                } */}
+
+                {
+                    isVisible && (
+                        <PopoverContainer 
+                            ref={ floating }
+                            style={ {
+                                position: strategy,
+                                top: y ?? '',
+                                left: x ?? '',
+                            } }
                         >
                             {content}
                         </PopoverContainer>
