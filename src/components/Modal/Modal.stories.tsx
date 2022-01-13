dba0bb40f858f437aeae5e5551c11ac2dd2c20ab
Daniel <daniilterawars@gmail.com>
2022-01-13 21:07:03 +0300

    feat(Modal): init

---
+++ src/components/Modal/Modal.stories.tsx
@@ -0,0 +1,10 @@
+import { Meta } from '@storybook/react';
+
+import {Modal} from './index';
+
+export default {
+    title: 'Components/Modal',
+    component: Modal,
+} as Meta;
+
+export { default as Default } from './__examples__/Default';
