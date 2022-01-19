e0440055bceafc22775295b0d39c194e4fefdb3d
Daniel <daniilterawars@gmail.com>
2022-01-19 20:59:42 +0300

    feat: add addons actions

--- src/components/Button/Button.stories.tsx
+++ src/components/Button/Button.stories.tsx
@@ -1,17 +1,19 @@
-import React from 'react';
-import { Story, Meta } from '@storybook/react';
+import React from 'react'
+import { Story, Meta } from '@storybook/react'
 
-import { Button, ButtonProps } from './index';
+import { Button, ButtonProps } from './index'
 
 export default {
-    title: 'Components/Button',
-    component: Button,
-} as Meta;
+  title: 'Components/Button',
+  component: Button,
+  argTypes: {
+    onClick: {
+      action: 'clicked',
+    },
+  },
+} as Meta
 
-const ButtonTemplate: Story<ButtonProps> = (args) => <Button { ...args } />;
-
-export { default as Default } from './examples/Default';
-export { default as Dark } from './examples/Dark';
-export { default as SuccessLarge } from './examples/SuccessLarge';
-export { default as Disabled } from './examples/Disabled';
-
+export { default as Default } from './examples/Default'
+export { default as Dark } from './examples/Dark'
+export { default as SuccessLarge } from './examples/SuccessLarge'
+export { default as Disabled } from './examples/Disabled'
--- src/components/Button/examples/Dark.tsx
+++ src/components/Button/examples/Dark.tsx
@@ -1,19 +1,21 @@
-import React, {useRef} from 'react';
-import {Button} from "../index";
+import React, { useRef } from 'react'
+import { Button } from '../index'
+import { action } from '@storybook/addon-actions'
 
 const Dark = (): JSX.Element => {
-    const btnRef = useRef<HTMLButtonElement>(null);
+  const btnRef = useRef<HTMLButtonElement>(null)
 
-    return (
-        <Button
-            ref={ btnRef }
-            color="dark"
-            outline
-            size="large"
-        >
-            Dark outlined button
-        </Button>
-    );
-};
+  return (
+    <Button
+      ref={ btnRef }
+      color="dark"
+      outline
+      size="large"
+      onClick={action('onClick')}
+    >
+      Dark outlined button
+    </Button>
+  )
+}
 
-export default Dark;
+export default Dark
