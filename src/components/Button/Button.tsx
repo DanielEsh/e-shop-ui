399ebd23fd305364fb69c70009108379e7dd0c43
Daniel <daniilterawars@gmail.com>
2022-01-19 22:12:05 +0300

    feat(Button): add props

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,15 +1,27 @@
-import { forwardRef } from 'react'
+import { forwardRef, ReactNode } from 'react'
 
 export type ButtonProps = {
+    children: ReactNode
+    addonLeft?: ReactNode
+    addonRight?: ReactNode
     color?: 'primary' | 'success' | 'warning' | 'danger' | 'info' | 'light' | 'dark'
-    size?: 'small' | 'medium' | 'large';
-    children: string;
-    outline?: boolean,
-    rounded?: boolean,
-    disabled?: boolean,
-    type?: 'button' | 'submit',
-    rippleEffect?: boolean,
-    onClick?: () => void;
+    size?: 'small' | 'medium' | 'large'
+    type?: 'button' | 'submit'
+    outline?: boolean
+    rounded?: boolean
+    disabled?: boolean
+    rippleEffect?: boolean
+    onMouseDown?: () => void
+    onMouseUp?: () => void
+    onMouseEnter?: () => void
+    onMouseLeave?: () => void
+    onClick?: () => void
+    onFocus?: () => void
+    onBlur?: () => void
+    className?: string
+    id?: string
+    title?: string
+    role?: string
 }
 
 export const Button = forwardRef<HTMLButtonElement, ButtonProps>((props, ref) => {
