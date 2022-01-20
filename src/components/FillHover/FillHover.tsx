56e6da648212a239f1ebf06a101ce35fb9c4cd96
Daniel <daniilterawars@gmail.com>
2022-01-20 23:17:42 +0300

    feat(FillHover): prototype

---
+++ src/components/FillHover/FillHover.tsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+
+const defaultCoords = {
+  x: 50,
+  y: 50,
+}
+
+export const FillHover = () => {
+  const [hoverPosition, setHoverPosition] = useState(defaultCoords)
+
+  const onHover = () => {
+    // get mouse position
+    // get hover position
+  }
+
+  return (
+    <div />
+  )
+}
