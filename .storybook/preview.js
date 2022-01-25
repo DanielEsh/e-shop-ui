5a93ef3f8b986039dfefee079ce2f85a580bfc8d
Daniel <daniilterawars@gmail.com>
2022-01-25 22:36:59 +0300

    feat: add dark mode

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,7 +1,22 @@
 import '../src/style.css'
+import { themes } from '@storybook/theming'
 
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
+  darkMode: {
+    // Override the default dark theme
+    dark: { ...themes.dark, appBg: 'black' },
+    // Override the default light theme
+    light: { ...themes.normal, appBg: 'white' },
+    // classes
+    darkClass: 'dark',
+    lightClass: 'light',
+    // class target
+    classTarget: 'html',
+    // Set the initial theme
+    current: 'light',
+    stylePreview: true,
+  },
   controls: {
     matchers: {
       date: /Date$/,
