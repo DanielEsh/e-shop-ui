62cc2adc36f2ff8572cdddb6d2996c13749c8d62
Daniel <daniilterawars@gmail.com>
2022-01-25 21:34:16 +0300

    feat: add colors

--- src/components/Colors/index.tsx
+++ src/components/Colors/index.tsx
@@ -12,6 +12,18 @@ const Colors = () => (
     <div className="bg-secondary-300 text-light">colors</div>
     <div className="bg-secondary-200 text-light">colors</div>
     <div className="bg-secondary-100 text-light">colors</div>
+    <h2>Gray</h2>
+    <div className="bg-gray-500">colors</div>
+    <div className="bg-gray-400">colors</div>
+    <div className="bg-gray-300">colors</div>
+    <div className="bg-gray-200">colors</div>
+    <div className="bg-gray-100">colors</div>
+    <h2>Warning</h2>
+    <div className="bg-warning">colors</div>
+    <h2>Success</h2>
+    <div className="bg-success">colors</div>
+    <h2>Danger</h2>
+    <div className="bg-danger">colors</div>
   </div>
 )
 
