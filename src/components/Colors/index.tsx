dae75e4706ab675de6290ff1ec673f19e4a34aed
Daniel <daniilterawars@gmail.com>
2022-01-25 23:04:51 +0300

    feat: add colors

--- src/components/Colors/index.tsx
+++ src/components/Colors/index.tsx
@@ -1,35 +1,45 @@
 const Colors = () => (
-  <div>
-    <h2>Primary</h2>
-    <div className="bg-primary-500 dark:bg-gray-500">colors</div>
-    <div className="bg-primary-400">colors</div>
-    <div className="bg-primary-300">colors</div>
-    <div className="bg-primary-200">colors</div>
-    <div className="bg-primary-100">colors</div>
-    <h2>Dark</h2>
-    <div className="bg-dark-500 text-white">colors</div>
-    <div className="bg-dark-400 text-white">colors</div>
-    <div className="bg-dark-300 text-white">colors</div>
-    <div className="bg-dark-200 text-white">colors</div>
-    <div className="bg-dark-100 text-white">colors</div>
-    <h2>Light</h2>
-    <div className="bg-light-500">colors</div>
-    <div className="bg-light-400">colors</div>
-    <div className="bg-light-300">colors</div>
-    <div className="bg-light-200">colors</div>
-    <div className="bg-light-100">colors</div>
-    <h2>Gray</h2>
-    <div className="bg-gray-500">colors</div>
-    <div className="bg-gray-400">colors</div>
-    <div className="bg-gray-300">colors</div>
-    <div className="bg-gray-200">colors</div>
-    <div className="bg-gray-100">colors</div>
-    <h2>Warning</h2>
-    <div className="bg-warning">colors</div>
-    <h2>Success</h2>
-    <div className="bg-success">colors</div>
-    <h2>Danger</h2>
-    <div className="bg-danger">colors</div>
+  <div className="flex flex-wrap">
+    <div className="w-96 mr-8 mb-4">
+      <h2 className="text-2xl">Primary</h2>
+      <div className="bg-primary-500">#fef3c7</div>
+      <div className="bg-primary-400">#fde68a</div>
+      <div className="bg-primary-300">#fcd34d</div>
+      <div className="bg-primary-200">#fbbf24</div>
+      <div className="bg-primary-100">#ffd139</div>
+    </div>
+    <div className="w-96 mr-8 mb-4">
+      <h2 className="text-2xl">Dark</h2>
+      <div className="bg-dark-500 text-white">#161616</div>
+      <div className="bg-dark-400 text-white">#1c1917</div>
+      <div className="bg-dark-300 text-white">#262626</div>
+      <div className="bg-dark-200 text-white">#404040</div>
+      <div className="bg-dark-100 text-white">#525252</div>
+    </div>
+    <div className="w-96 mr-8 mb-4">
+      <h2 className="text-2xl">Light</h2>
+      <div className="bg-light-500">#fafafa</div>
+      <div className="bg-light-400">#f4f4f5</div>
+      <div className="bg-light-300">#e4e4e7</div>
+      <div className="bg-light-200">#d4d4d8</div>
+      <div className="bg-light-100">#a1a1aa</div>
+    </div>
+    <div className="w-96 mr-8 mb-4">
+      <h2 className="text-2xl">Gray</h2>
+      <div className="bg-gray-500">#e5e7eb</div>
+      <div className="bg-gray-400">#d1d5db</div>
+      <div className="bg-gray-300">#9ca3af</div>
+      <div className="bg-gray-200">#d1d5db</div>
+      <div className="bg-gray-100">#e5e7eb</div>
+    </div>
+    <div className="w-96 mr-8 mb-4">
+      <h2 className="text-2xl">Warning</h2>
+      <div className="bg-warning">#fbbf24</div>
+      <h2 className="text-2xl">Success</h2>
+      <div className="bg-success">#22c55e</div>
+      <h2 className="text-2xl">Danger</h2>
+      <div className="bg-danger">#dc2626</div>
+    </div>
   </div>
 )
 
