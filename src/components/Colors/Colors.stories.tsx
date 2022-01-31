ff82d84409b093f7d6fb7d128f11f3efcd600a58
Daniel <daniilterawars@gmail.com>
2022-01-31 21:58:51 +0300

    feat: Colors

--- src/components/Colors/Colors.stories.tsx
+++ src/components/Colors/Colors.stories.tsx
@@ -1,14 +1,14 @@
-import React from 'react';
-import { Meta } from '@storybook/react';
+import React from 'react'
+import { Meta } from '@storybook/react'
 
-import Colors from "./index";
+import Colors from './index'
 
 export default {
-    title: 'Example/Colors',
-    component: Colors,
-} as Meta;
+  title: 'Colors',
+  component: Colors,
+} as Meta
 
-const Template = (args) => <Colors { ...args } />;
+const Template = (args) => <Colors { ...args } />
 
-export const All = Template.bind({});
-All.args = {};
+export const All = Template.bind({})
+All.args = {}
