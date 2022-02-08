3d1087ffc3972964d614e927464b17c5c332544c
Daniel <daniilterawars@gmail.com>
2022-02-08 23:16:36 +0300

    feat(Checkbox): prototype

--- src/components/_new/Checkbox/Checkbox.tsx
+++ src/components/_new/Checkbox/Checkbox.tsx
@@ -2,12 +2,30 @@ import { ReactNode, forwardRef } from 'react'
 
 export interface CheckboxProps {
     children: ReactNode
+    label?: string
+    className?: string
+    color?: 'primary' | 'secondary' | 'gray' | 'success' | 'error'
+    size?: 'small' | 'medium' | 'large'
+    disabled?: boolean
+    checked?: boolean
+    onChange?: (event) => void
+    onClick?: () => void
+    onFocus?: () => void
+    onBlur?: () => void
+    id?: string
+    name?: string
 }
 
 export const Checkbox = forwardRef<HTMLInputElement, CheckboxProps>((props, ref) => {
   return (
-    <div ref={ref}>
-      {props.children}
+    <div className="flex items-center">
+      <input
+        ref={ref}
+        type="checkbox"
+      />
+      <div>
+        {props.children}
+      </div>
     </div>
   )
 })
