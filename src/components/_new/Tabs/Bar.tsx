17cf276c967a6e24f53b15383a0671bb5a926b95
Daniel <daniilterawars@gmail.com>
2022-02-26 17:30:52 +0300

    feat(Tab): focus

--- src/components/_new/Tabs/Bar.tsx
+++ src/components/_new/Tabs/Bar.tsx
@@ -1,20 +1,16 @@
 import { useState, useContext, useEffect, useRef } from 'react'
 import cn from 'classnames'
-import { focusElement, focusIn, Focus } from '@/utils/focus-management'
+import { focusIn, Focus } from '@/utils/focus-management'
 import { keyList, isKeyCode } from '@/utils/isKeyCode'
 import { TabsContext } from '@/components/_new/Tabs/Tabs'
 import { Indicator } from '@/components/_new/Tabs/Indicator'
-
-import { useFocusTrap } from '@/hooks/useFocusTab'
 
 export const Bar = ({ children }) => {
   const [boundingActiveTab, setBoundingActiveTab] = useState(null)
   const barRef = useRef<HTMLDivElement>(null)
   const tabs = useRef<any>(null)
 
-  const focusTrapRef = useFocusTrap()
-
-  const { activeTab, onChange, color } = useContext(TabsContext)
+  const { activeTab, color } = useContext(TabsContext)
 
   const colorsList = {
     primary: 'bg-dark-500 ',
@@ -26,18 +22,7 @@ export const Bar = ({ children }) => {
     colorsList[color],
   )
 
-  let index = 0
-
-  const getNextFocusedNodeIndex = (direction: number) => {
-    index += direction
-    if (index > (tabs.current.length - 1)) index = 0
-    if (index < 0) index = tabs.current.length - 1
-
-    return index
-  }
-
   const onKeyDown = (event) => {
-    console.log(isKeyCode(event.code, [keyList.LEFT, keyList.RIGHT]))
     if (isKeyCode(event.code, [keyList.LEFT])) {
       return focusIn(barRef.current, Focus.Previous | Focus.WrapAround)
     }
@@ -45,17 +30,6 @@ export const Bar = ({ children }) => {
     if (isKeyCode(event.code, [keyList.RIGHT])) {
       focusIn(barRef.current, Focus.Next | Focus.WrapAround)
     }
-    // let direction = 0
-    // if (event.code === 'ArrowRight') {
-    //   direction += 1
-    // }
-
-    // if (event.code === 'ArrowLeft') {
-    //   direction += -1
-    // }
-
-    // const focusIndex = getNextFocusedNodeIndex(direction)
-    // focusElement(tabs.current[focusIndex])
   }
 
   useEffect(() => {
@@ -84,10 +58,7 @@ export const Bar = ({ children }) => {
       ref={barRef}
       className={classes}
     >
-      <div
-        className="relative flex"
-        ref={focusTrapRef}
-      >
+      <div className="relative flex">
         {boundingActiveTab && <Indicator styles={styles} />}
         {children}
       </div>
